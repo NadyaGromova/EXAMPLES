cbf61ee442e75f3a0c4366e184a19bb477398599
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:36:32 +0300

    Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти максимальное из 5 чисел.
+
+int a = 1;
+int b = 22;
+int c = 3;
+int d = 4;
+int e = 6;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine($"max = {max}");
+
+
