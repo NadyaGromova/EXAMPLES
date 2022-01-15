c696befb3c6575b43101046d2e131a48c30f5f92
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-15 17:38:45 +0300

    Example012.1_SortMassive

---
+++ Example012.1_SortMassive/Program.cs
@@ -0,0 +1,56 @@
+﻿int[] arr = { 1, 25, 3, 34, 50, 6, 7, 7 };
+
+
+// метод печати массива:
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+
+// метод сортировки от минимального к максимальному:
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+SelectionSort(arr);
+PrintArray(arr);
+
+// метод сортировки массива от максимального к минимальному:
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
+    }
+}
+
+SelectionSortMax (arr);
+PrintArray(arr);
