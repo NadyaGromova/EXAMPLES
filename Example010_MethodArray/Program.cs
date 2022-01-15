dcd0f410e0e219f67dd107e5279069b9fd916b8e
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-15 16:30:46 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 2, 3, 4, 82, 52, 46, 37, 82 };
+
+int n = array.Length;
+int find = 82;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
